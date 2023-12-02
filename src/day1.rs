b07f16ae3cd6018d8dbca0cdd014b3e23492cd8e
Henning Merklinger <henning.merklinger@gmail.com>
2023-12-02 22:59:00 +0100

    refactor day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,11 +1,18 @@
 use crate::utils::input;
 
+fn inputs() -> Vec<String> {
+    input(&1).split("\n").map(|line| line.to_string()).collect()
+}
+
+fn get_number(s: &String) -> i32 {
+    let arr: Vec<char> = s.replace(char::is_alphabetic, "").chars().collect();
+    format!("{}{}", arr[0], arr[arr.len()-1]).parse::<i32>().unwrap()
+}
+
 pub fn task1() {
     let mut result: i32 = 0;
-    for row in input(&1).split("\n").collect::<Vec<&str>>().iter() {
-        let a: Vec<char> = row.replace(char::is_alphabetic, "").chars().collect::<Vec<char>>();
-        result += format!("{}{}", a[0], a[a.len()-1]).parse::<i32>().unwrap();
-        // println!("{:?}", format!("{}{}", a[0], a[a.len()-1]).parse::<i32>().unwrap());
+    for row in inputs().iter() {
+        result += get_number(row);
     }
     println!("Result 1: {:?}", result);
 }
@@ -13,14 +20,12 @@ pub fn task1() {
 pub fn task2() {
     let mut result: i32 = 0;
     let numbers: [&str; 10] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    for row in input(&1).split("\n").collect::<Vec<&str>>().iter() {
+    for row in inputs().iter() {
         let mut x: String = row.to_string();
         for (i, number) in numbers.iter().enumerate() {
             x = x.replace(number, format!("{}{}{}", number, i, number).as_str());
         }
-        let a: Vec<char> = x.replace(char::is_alphabetic, "").chars().collect::<Vec<char>>();
-        result += format!("{}{}", a[0], a[a.len()-1]).parse::<i32>().unwrap();
-        // println!("{:?}", format!("{}{}", a[0], a[a.len()-1]).parse::<i32>().unwrap());
+        result += get_number(&x);
     }
     println!("Result 2: {:?}", result);
 }
