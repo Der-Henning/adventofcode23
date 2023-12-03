34974daca5c37e924e4d4962ddf983ecebc3b9f1
Henning Merklinger <henning.merklinger@gmail.com>
2023-12-03 14:23:13 +0100

    added tests

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,27 @@ fn main() {
     print_results(2, day2::task1(), day2::task2());
     print_results(3, day3::task1(), day3::task2());
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day1() {
+        assert_eq!(day1::task1(), 55386);
+        assert_eq!(day1::task2(), 54824);
+    }
+
+    #[test]
+    fn day2() {
+        assert_eq!(day2::task1(), 1931);
+        assert_eq!(day2::task2(), 83105);
+    }
+
+    #[test]
+    fn day3() {
+        assert_eq!(day3::task1(), 498559);
+        assert_eq!(day3::task2(), 72246648);
+    }
+}
